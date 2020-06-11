c15cfb0f5fcdc66b67fac3686d6d8e6ae62f5efb
jujulili1 <59768244+jujulili1@users.noreply.github.com>
2020-06-11 12:24:29 +0700

    Add files via upload

---
+++ latihan8/soal1.js
@@ -0,0 +1,14 @@
+Name = 'ian';
+console.log(Name);
+
+let Address = 'Klaten';
+console.log(Address);
+
+let birthDate = '9 juli 1995';
+console.log(birthDate);
+
+let Age = 24;
+console.log(Age);
+
+let Profesion = 'impactbyte';
+console.log(Profesion)
